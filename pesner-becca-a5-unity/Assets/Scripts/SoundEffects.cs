ba128af98ac3932894eac9d892e7feb233f58a5e
beccapesner <000351580@mohawkcollege.ca>
2025-04-15 13:13:07 -0400

    omg fixing more code lmaooooo

--- pesner-becca-a5-unity/Assets/Scripts/SoundEffects.cs
+++ pesner-becca-a5-unity/Assets/Scripts/SoundEffects.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SoundEffects : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializedField] private AudioSource _audioSource;
+    [SerializeField] private AudioSource _audioSource;
 
     // Update is called once per frame
     void Update()
